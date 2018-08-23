c14b1c87f6e0410d97f8011fee01b1b05d688de1
Tamara <tjarataibo@gmail.com>
2018-08-23 09:28:51 -0300

    Edición de nombre de archivos

---
+++ test/md-links.spec.js
@@ -0,0 +1,5 @@
+// Debe contener los tests unitarios para la función mdLinks()tu implementación debe pasar estos tests.
+
+const assert = require('chai').assert;
+global.window = global;
+require('validate.js');
--- test/validate.specs.js
+++
@@ -1,3 +0,0 @@
-const assert = require('chai').assert;
-global.window = global;
-require('validate.js');
