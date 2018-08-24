e667017ebea70d23f6c1a66cc27067dd87c08866
Tamara <tjarataibo@gmail.com>
2018-08-24 12:11:50 -0300

    Comentarios en test

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,14 +1,13 @@
 // Debe contener los tests unitarios para la función mdLinks()tu implementación debe pasar estos tests.
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-const expect = require('expect.js');
-const http = require('http');
-const express = require('express');
-const markdownLinkCheck = require('../');
-
-const assert = require('chai').assert;
+const fs = require('fs'); // fs roporciona una API para interactuar con el sistema de archivos de una manera estrechamente modelada alrededor de las funciones POSIX estándar.
+const path = require('path'); // Path es un  módulo que proporciona utilidades para trabajar con rutas de archivos y directorios.
+const expect = require('expect.js'); 
+const http = require('http'); 
+const express = require('express'); // Ayudantes HTTP (redirección, almacenamiento en caché, etc.)
+const markdownLinkCheck = require('../'); // Muestra los links e imágenes viables o rotos
+const assert = require('chai').assert; // Validación de test
 global.window = global;
 require('validate.js');
 
