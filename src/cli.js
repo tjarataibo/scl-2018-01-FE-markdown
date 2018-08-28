826f0490154c1a8cf750e5f176940432f8ed4922
Tamara <tjarataibo@gmail.com>
2018-08-28 11:24:41 -0300

    Cambio en constantes

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // EJEMPLO: function(a, b, ...theArgs). Si el último argumento definido de una función se le antepone con ..., éste se convierte en un arreglo cuyos elementos desde 0 (inclusivo) hasta theArgs.length (exclusivo) son pasados a la función.
-const [,, ...argvs] = process.argv; // Captará todos los argumentos dados de línea de comando. process.argv devuelve una matriz que contiene los argumentos de línea de comando pasados ​​cuando se inició el proceso Node.js. 
+const [,, ...argvs] = process.argv; // Operador "esparcir". Captará todos los argumentos dados de línea de comando. process.argv devuelve una matriz que contiene los argumentos de línea de comando pasados ​​cuando se inició el proceso Node.js. 
 const resultArgvs = argvs.length;
 const path = argvs[0];
 const mdLinks = require('./app');
@@ -17,20 +17,25 @@ representa un link con 4 propiedades (href, text, file y line) */
 if (resultArgvs === 1) {
   mdLinks(path, options) /*  interfaz que ofrece -path: Ruta absoluta o relativa al archivo. -options: Un objeto con la siguiente propiedad: 
   validate: Valor que determina si se desea validar los links encontrados en el archivo. */
+
+  // El método forEach() ejecuta la función indicada una vez por cada elemento del array
     .then(response => response.forEach(({file, href, text, line}) => console.log(`${file}:${line} ${href} ${text}`)));
 } else if (resultArgvs === 2 && argvs[1] === '--validate') {
   options.validate = true;
   mdLinks(path, options)
     .then(response => response.forEach(({file, href, text, line, validate}) => console.log(`${file}:${line} ${href} ${validate.statusText} ${validate.status} ${text}`)));
+
 } else if (resultArgvs === 2 && argvs[1] === '--stats') {
   options.stats = true;
   mdLinks(path, options)
     .then(response => console.log(response));
+
 } else if (resultArgvs === 3 && argvs[1] === '--validate' && argvs[2] === '--stats') {
   options.validate = true;
   options.stats = true;
   mdLinks(path, options)
     .then(response => console.log(response));
+    
 } else {
   console.log('Comando no válido');
 }
